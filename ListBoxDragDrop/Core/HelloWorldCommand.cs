7592798d616c657eb57e4c3a530b72979b55e55b
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-10-06 20:25:25 +0200

    *

---
+++ ListBoxDragDrop/Core/HelloWorldCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ListBoxDragDrop.Core
+{
+    using System.Windows;
+
+    using ListBoxDragDrop.Extension;
+
+    public class HelloWorldCommand : CommandExtension<HelloWorldCommand>
+    {
+        public override void Execute(object parameter)
+        {
+            MessageBox.Show("Hello world.");
+        }
+    }
+}
